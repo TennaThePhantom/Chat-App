f63478e0cdadfa46063d816023bcade1d416ae6f
Tennessee Foster <tennatenna99@outlook.com>
2025-10-03 18:12:18 -0400

    DaisyUi setup and router dom

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className='text-red-500'>App</div>
-  )
-}
+	return (
+		<div className="text-red-500">
+			<button className="btn btn-neutral">Neutral</button>
+			<button className="btn btn-primary">Primary</button>
+			<button className="btn btn-secondary">Secondary</button>
+			<button className="btn btn-accent">Accent</button>
+			<button className="btn btn-info">Info</button>
+			<button className="btn btn-success">Success</button>
+			<button className="btn btn-warning">Warning</button>
+			<button className="btn btn-error">Error</button>
+		</div>
+	);
+};
 
-export default App
+export default App;
